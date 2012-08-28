483342420576a42497c29f37e88acfa033dd7e08
Denis Gorin <nmcoder@gmail.com>
2012-08-28 11:02:44 +0300

    Update explorers

--- app/components/project_issue_explorer.rb
+++ app/components/project_issue_explorer.rb
@@ -1,16 +1,23 @@
 # Component that allows exploring issues per project (using 2 grids). Inherits from the same Netzke::Communitypack::OneToManyExplorer as UserIssueExplorer.
 class ProjectIssueExplorer < Netzke::Communitypack::OneToManyExplorer
-  title "Projects/Issues"
+  def configure(c)
+    c.title = "Project/Issues"
+    c.foreign_key = :project_id
+    super
+  end
 
-  container_config ({
-    region: :north,
-    class_name: "ProjectGrid"
-  })
+  component :container do |c|
+    c.klass = ProjectGrid
+    c.region = :north
+    c.height = 200
+    super c
+  end
 
-  collection_config ({
-    class_name: "IssueGrid",
-    override_columns: {
+  component :collection do |c|
+    c.klass = IssueGrid
+    c.override_columns = {
       project__name: {included: false} # we don'n need this column in ProjectIssueExplorer
     }
-  })
+    super c
+  end
 end
--- app/components/user_issue_explorer.rb
+++ app/components/user_issue_explorer.rb
@@ -1,30 +1,24 @@
 # Component that allows exploring issues per user (using 2 grids). Inherits from the same Netzke::Communitypack::OneToManyExplorer as ProjectIssueExplorer.
 class UserIssueExplorer < Netzke::Communitypack::OneToManyExplorer
-  title "Users/Issues"
-
-  association :assignee # association that binds users and issues
-
-  #container_config  class_name: "UserGrid", # our custom UserGrid as container grid
-                    #region: :north, # we want it on top
-                    #split: true
-
-  #collection_config class_name: "IssueGrid", override_columns: {
-    #assignee__name: {included: false} # we don'n need this column in ProjectIssueExplorer
-  #}
-
-  def container_component(c)
+  def configure(c)
+    c.title = "Users/Issues"
+    c.foreign_key = :assignee_id
     super
+  end
+
+  component :container do |c|
     c.klass = UserGrid
     c.region = :north
+    c.height = 200
     c.split = true
+    super c
   end
 
-  def collection_component(c)
-    super
+  component :collection do |c|
     c.klass = IssueGrid
-    c.override_columns: {
+    c.override_columns = {
       assignee__name: {included: false} # we don'n need this column in ProjectIssueExplorer
     }
+    super c
   end
-
 end

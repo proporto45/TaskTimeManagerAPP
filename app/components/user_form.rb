8d5ac03e35cb524e1f16e3db6d50c1badbc961f1
Denis Gorin <nmcoder@gmail.com>
2012-09-27 09:05:36 +0300

    Update UserInspector

--- app/components/user_form.rb
+++ app/components/user_form.rb
@@ -1,25 +1,28 @@
 # Custom user form (predefined model and layout)
 class UserForm < Netzke::Basepack::FormPanel
-  model "User"
+  def configure(c)
+    c.record = User.first # default user id - handy for individual component testing
 
-  record_id User.first.try(:id) # default user id - handy for individual testing
+    super
 
-  items [
-    {
-      layout: :hbox,
-      border: false,
-      defaults: {border: false}, 
-      items: [{
-        flex: 1,
-        layout: :anchor,
-        defaults: {anchor: "-8"},
-        items: [:name, :email]
-      },{
-        flex: 1,
-        layout: :anchor,
-        defaults: {anchor: "100%"},
-        items: [:number_of_issues, :number_of_open_issues]
-      }]
-    }
-  ]
+    c.model = "User"
+    c.items = [
+      {
+        layout: :hbox,
+        border: false,
+        defaults: {border: false},
+        items: [{
+          flex: 1,
+          layout: :anchor,
+          defaults: {anchor: "-8"},
+          items: [:name, :email]
+        },{
+          flex: 1,
+          layout: :anchor,
+          defaults: {anchor: "100%"},
+          items: [:number_of_issues, :number_of_open_issues]
+        }]
+      }
+    ]
+  end
 end
